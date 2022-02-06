a56c4e8456cb00f146f275a9b72605b7c631098a
metamodernity <95966082+metamodernity@users.noreply.github.com>
2022-02-06 19:01:14 +0300

    Update and rename Program.cs to Hello World!.cs

--- Hello World!.cs
+++ Hello World!.cs
@@ -1,12 +1,11 @@
-﻿using System;
+using System;
 
 namespace HelloWorld
 {
     class first
     {
         static void Main(string[] args)
-        {
-            
+        { 
             Console.Write("Hello World!");
         }
     }
