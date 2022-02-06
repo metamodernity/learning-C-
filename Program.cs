55d049c0fe6e9ebb527ae673eb83be868ce5fe30
metamodernity <95966082+metamodernity@users.noreply.github.com>
2022-02-06 18:56:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class first
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Hello World!");
+        }
+    }
+}
